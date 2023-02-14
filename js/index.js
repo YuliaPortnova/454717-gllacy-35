2ce3bfdbe93e298efb17ce52a8cba29588907112
YuliaPortnova <yuyul@yandex.ru>
2023-02-14 13:36:01 +0300

    Уточняет величину отступов по PixelPerfect

--- js/index.js
+++ js/index.js
@@ -76,3 +76,6 @@ paginations.forEach((element, index) => {
   });
 })
 
+const searchResetButton = document.querySelector('.search-reset-button');
+const searchInput = document.querySelector('.search-input');
+searchResetButton.addEventListener('click', () => searchInput.value = ' ');
