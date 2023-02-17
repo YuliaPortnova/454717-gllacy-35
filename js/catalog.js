d0377e710d620252eff38e4c396787c809faad22
YuliaPortnova <yuyul@yandex.ru>
2023-02-17 23:12:04 +0300

    Оптимизирует js, чтобы убрать лишний файл

--- js/catalog.js
+++
@@ -1,6 +0,0 @@
-const searchResetButton = document.querySelector('.search-reset-button');
-const searchInput = document.querySelector('.search-input');
-searchResetButton.addEventListener('click', () => {
-  searchInput.value = '';
-  searchInput.focus();
-});
